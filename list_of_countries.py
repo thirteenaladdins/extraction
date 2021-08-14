e3ce37858c1e4f2ddb0342df21da0c3d7effdfee
thirteenaladdins <thirteenaladdins@gmail.com>
2021-08-14 11:45:37 +0100

    fresh branch

--- list_of_countries.py
+++
@@ -1,9 +0,0 @@
-# add different abbreviations here?
-
-EU_27 = [
-    "Austria", "Italy", "Belgium", "Latvia", "Bulgaria", "Lithuania",
-    "Croatia", "Luxembourg", "Cyprus", "Malta", "Czech Republic", "Netherlands",
-    "Denmark", "Poland", "Estonia", "Portugal", "Finland", "Romania",
-    "France",	"Slovakia", "Germany", "Slovenia", "Greece", "Spain",
-    "Hungary",	"Sweden", "Ireland",
-]
